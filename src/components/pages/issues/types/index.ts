8234e427dea55947d65ec181b6fb718598f86b1d
Sarad Singh <theArkein.dev@gmail.com>
2023-05-28 18:21:54 +0545

    Make closed_at property in IssueType optional

--- src/components/pages/issues/types/index.ts
+++ src/components/pages/issues/types/index.ts
@@ -2,8 +2,8 @@ export interface IssueType {
     id: number,
     number: number,
     title: string,
-    created_at: Date,
-    closed_at: Date,
+    created_at: Date | string,
+    closed_at?: Date | string,
     state: "open" | "closed",
     user: {
         login: string,
